fd5918ff44ce5a933b9411ab60a19d17f07372a4
RickJY <98106012+RickJY@users.noreply.github.com>
2023-11-30 08:57:25 -0700

    Update upload.py
    Due to software permission problem, this script will continue running to compare the specify path which has new file or not. if there is a new file, it will upload and send a notification.

--- upload.py
+++ upload.py
@@ -2,6 +2,7 @@ from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
 import os
 import glob
+import time
 
 # Google drive api auth
 gauth = GoogleAuth()
@@ -12,29 +13,45 @@ drive = GoogleDrive(gauth)
 directory_path = '/home/pi/Pictures'
 folder_id = '1GXcT1o8iFkjG6WV53lY_lk-Fu32pvFQ7'
 
-# To check the file is exist or not
-def find_latest_jpg(path):
+# To check if there is a new file
+def find_latest_jpg(path, last_checked_time):
     jpg_files = glob.glob(os.path.join(path, '*.jpg'))
-    # If there is not a JPG image, it will stop
+
+    # If there are no JPG files, it will stop
     if not jpg_files:
         print(f"No JPG files found in {path}")
-        return None
-    latest_png = max(jpg_files, key=os.path.getmtime)
-    # Return the path 
-    return latest_png
+        return None, last_checked_time
 
-# Call the function to find and print the latest JPG file
-latest_image_path = find_latest_jpg(directory_path)
-if latest_image_path:
-    print(f"Latest image file: {latest_image_path}")
-    f = drive.CreateFile({
-        'title': os.path.basename(latest_image_path),
-        'mimeType': 'image/jpeg', 
-        'parents': [{'kind': 'drive#fileLink', 'id': folder_id}]
-    })
-    f.SetContentFile(latest_image_path)
-    f.Upload()
-    print("File uploaded to Google Drive.")
-else:
-    print("No image file found.")
+    # Find the latest file based on modification time
+    latest_jpg = max(jpg_files, key=os.path.getmtime)
+    latest_mtime = os.path.getmtime(latest_jpg)
 
+    # Check if the latest file is newer than the last checked time
+    if latest_mtime > last_checked_time:
+        print(f"New image file found: {os.path.basename(latest_jpg)}")
+        return latest_jpg, latest_mtime
+    else:
+        print("No new image files.")
+        return None, last_checked_time
+
+# Initialize the last checked time with the current time
+last_checked_time = time.time()
+
+# Periodically check for new files (adjust the sleep duration as needed)
+while True:
+    latest_image_path, last_checked_time = find_latest_jpg(directory_path, last_checked_time)
+    
+    if latest_image_path:
+        print(f"Latest image file: {latest_image_path}")
+        f = drive.CreateFile({
+            'title': os.path.basename(latest_image_path),
+            'mimeType': 'image/jpeg', 
+            'parents': [{'kind': 'drive#fileLink', 'id': folder_id}]
+        })
+        f.SetContentFile(latest_image_path)
+        f.Upload()
+        print("File uploaded to Google Drive.")
+        os.system('python3 AWS_connect.py')
+
+    # Adjust the sleep duration based on your desired frequency of checking for new files
+    time.sleep(10)  # Sleep for 60 seconds before checking again
